4032c398d58e5b777929d60cd00b17f39125adbb
JoseAl05 <jparancibialinker@gmail.com>
2022-04-25 19:08:00 -0400

    compacted app file

--- backend/app.js
+++ backend/app.js
@@ -2,53 +2,24 @@ const express = require('express');
 const path = require('path');
 const morgan = require('morgan');
 const appRouter = require("./routes/router");
-const cors = require("cors");
-const session = require('express-session');
-const passport = require('passport');
 const csrf = require('csurf');
-const MongoStore = require('connect-mongo');
-const User = require("./models/User");
-const clientDb = require('./db');
 const { cookie } = require('express-validator');
+const setUpSession = require('./middleware/sessions');
+const setUpPassport = require('./middleware/passport');
+const setUpCors = require('./middleware/cors');
 
 
-var app = express();
+const app = express();
 
 
 require('dotenv').config()
-app.use(
-  cors({
-    credentials:true,
-    origin:'http://localhost:3000',
-  })
-);
+setUpCors(app);
+setUpSession(app);
+app.use(csrf({
+  cookie:false
+}));
+setUpPassport(app);
 
-app.use(session({
-  secret:process.env.SESSION_KEY,
-  resave:false,
-  saveUninitialized:false,
-  store:MongoStore.create({
-    clientPromise:clientDb,
-    dbName:process.env.DB_NAME,
-    touchAfter: 24 * 3600,
-    autoRemove: 'native'
-  }),
-  cookie:{
-    maxAge:15 * 24 * 60 *60 * 1000
-  }
-}))
-
-app.use(csrf());
-app.use(passport.initialize());
-app.use(passport.session());
-
-passport.serializeUser((user,done) =>
-  done(null,{id:user._id,username:user.username})
-)
-passport.deserializeUser(async(user,done) =>{
-  const userDB = await User.findById(user.id);
-  return done(null,{id:userDB._id,username:userDB.username});
-})
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({extended:true}));
